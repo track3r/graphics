52a16c3bffe5eaa5ed2532afc5c957554ec95f20
Mad <mad.paia@gmail.com>
2014-06-18 16:07:48 +0200

    Adding flash render target

---
+++ graphics/RenderTarget.hx
@@ -0,0 +1,33 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: epai
+ * Date: 04/06/14
+ * Time: 17:35
+ */
+package graphics;
+
+import types.SizeI;
+import types.Color4B;
+import graphics.GraphicsTypes;
+
+class RenderTarget
+{
+    public var size : SizeI;
+
+    public var colorFormat : ColorFormat;
+    public var depthFormat : DepthFormat;
+    public var stencilFormat : StencilFormat;
+
+    public var colorTextureData : TextureData;
+    public var depthTextureData : TextureData;
+    public var stencilTextureData : TextureData;
+
+    public var currentClearColor : Color4B;
+
+
+    public function new () : Void
+    {
+        currentClearColor = new Color4B();
+    }
+
+}
