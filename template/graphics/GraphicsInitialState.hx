e9b6c5a18c21698c6538c0d8eca9c028b8eac443
jxav <joao.xavier@gameduell.de>
2015-01-16 16:58:35 +0100

    Made the graphics initial state constant

--- template/graphics/GraphicsInitialState.hx
+++ template/graphics/GraphicsInitialState.hx
@@ -2,8 +2,8 @@ package graphics;
 
 class GraphicsInitialState
 {
-	public static var clearColorRed: Float  	= ::LIBRARY.GRAPHICS.CLEAR_COLOR_R::;
-	public static var clearColorGreen: Float  	= ::LIBRARY.GRAPHICS.CLEAR_COLOR_G::;
-	public static var clearColorBlue: Float  	= ::LIBRARY.GRAPHICS.CLEAR_COLOR_B::;
-	public static var clearColorAlpha: Float  	= ::LIBRARY.GRAPHICS.CLEAR_COLOR_A::;
+	public static inline var clearColorRed:   Float     = ::LIBRARY.GRAPHICS.CLEAR_COLOR_R::;
+	public static inline var clearColorGreen: Float  	= ::LIBRARY.GRAPHICS.CLEAR_COLOR_G::;
+	public static inline var clearColorBlue:  Float  	= ::LIBRARY.GRAPHICS.CLEAR_COLOR_B::;
+	public static inline var clearColorAlpha: Float  	= ::LIBRARY.GRAPHICS.CLEAR_COLOR_A::;
 }
